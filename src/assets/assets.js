c1c7f96fa22613557e212da70965166c3c16de0d
Dharmendra Kumar <dharmendracgs1234@gmail.com>
2025-05-27 01:56:32 +0530

    first commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -12,7 +12,7 @@ import sofa from './sofa.svg'
 import table from './table.svg'
 import almira from './almira.svg'
 import dining from './dining.svg'
-import Dresing from './dresing.svg'
+import dressing from './Dressing.svg';  // ✅ correct
 import teapai from './teapai.svg'
 import puja from './puja.svg'
 import trending1 from './trending_1.jpg'
@@ -40,7 +40,7 @@ export const assets = {
     table,
     almira,
     dining,
-    Dresing,
+    dressing, // ✅ correct
     puja,
     teapai,
     trending1,
